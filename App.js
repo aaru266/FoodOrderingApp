13b916df0f8156d4ca0023dc6b6e5b6265f355ff
Aaradhaya Khandelwal <aaradhayakhandelwal@gmail.com>
2023-03-29 14:23:36 +0530

    home page

--- App.js
+++ App.js
@@ -1,24 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text,Image,SafeAreaView,View,Pressable} from 'react-native';
+import chefImg from './assets/chef.jpg'
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>this project is made by aryan nama</Text>
-      <Text style={styles.text} >Hello there</Text>
+    <SafeAreaView style={styles.container}>
+      <Image source={chefImg} style={{width:400,height:500}} />
+      <View style={{padding:20}}>
+
+      <Text style={{fontSize:30,fontWeight:'bold',textAlign:"center"}}>The Fastest in Delivery <Text style={{color:"orange"}}>Food</Text></Text>
+      
+      </View>
+      <View>
+      <Text style={{marginTop:15,fontSize:20,color:"rgba(0,189,0,0.9)"}}>Giving your Hunger a new Option</Text>
+      </View>
+      <Pressable title='Get Started' style={styles.button}  >
+        <Text style={{fontSize:17,fontWeight:400}}> Get Started</Text>
+      </Pressable>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'blue',
+    backgroundColor: 'black',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
   },
-  text:{
-    color:"red"
+  button:{
+      backgroundColor:"tomato",
+      borderRadius:50,
+      marginTop:20,
+      padding:20,
+      paddingHorizontal:40,
+
+
   }
+  
 });
