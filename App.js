2db656c9a81f3f6cc0a3fb8cea5e152aa519f1e3
aryan nama <aryan.nama001@gmail.com>
2023-04-01 18:13:21 +0530

    commit -aryan

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text,Image,SafeAreaView,View,Pressable} from 'react-native';
+import { StyleSheet, Text, Image, SafeAreaView, View, Pressable } from 'react-native';
 import chefImg from './assets/chef.jpg'
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Image source={chefImg} style={{width:400,height:500}} />
-      <View style={{padding:20}}>
+      <Image source={chefImg} style={{ width: 400, height: 500 }} />
+      <View style={{ padding: 20 }}>
 
-      <Text style={{fontSize:30,fontWeight:'bold',textAlign:"center"}}>The Fastest in Delivery <Text style={{color:"orange"}}>Food</Text></Text>
-      
+        <Text style={{ fontSize: 30, fontWeight: 'bold', textAlign: "center" }}>The Fastest in Delivery <Text style={{ color: "orange" }}>Food</Text></Text>
+
       </View>
       <View>
-      <Text style={{marginTop:15,fontSize:20,color:"rgba(0,189,0,0.9)"}}>Giving your Hunger a new Option</Text>
+        <Text style={{ marginTop: 15, fontSize: 20, color: "rgba(0,189,0,0.9)" }}>Giving your Hunger a new Option</Text>
       </View>
       <Pressable title='Get Started' style={styles.button}  >
-        <Text style={{fontSize:17,fontWeight:400}}> Get Started</Text>
+        <Text style={{ fontSize: 17, fontWeight: 400 }}> Get Started</Text>
       </Pressable>
+      <Text>Updated by Aryan Nama</Text>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -28,14 +29,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     // justifyContent: 'center',
   },
-  button:{
-      backgroundColor:"tomato",
-      borderRadius:50,
-      marginTop:20,
-      padding:20,
-      paddingHorizontal:40,
+  button: {
+    backgroundColor: "tomato",
+    borderRadius: 50,
+    marginTop: 20,
+    padding: 20,
+    paddingHorizontal: 40,
 
 
   }
-  
+
 });
