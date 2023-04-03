2eba0476b80e45163b6339d2eb5b497d937f0637
Aaradhaya Khandelwal <aaradhayakhandelwal@gmail.com>
2023-04-03 11:22:14 +0530

    aaradhaya

---
+++ App.js
@@ -0,0 +1,36 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, Image, SafeAreaView, View, Pressable } from 'react-native';
+import chefImg from './assets/chef.jpg'
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Image source={chefImg} style={{ width: 400, height: 500 }} />
+      <View style={{ padding: 20 }}>
+
+        <Text style={{ fontSize: 30, fontWeight: 'bold', textAlign: "center" }}>The Fastest in Delivery <Text style={{ color: "orange" }}>Food</Text></Text>
+
+      </View>
+      <View>
+        <Text style={{ marginTop: 15, fontSize: 20, color: "rgba(0,189,0,0.9)" }}>Giving your Hunger a new Option</Text>
+      </View>
+      <Pressable title='Get Started' style={styles.button}  >
+        <Text style={{ fontSize: 17, fontWeight: 400 }}> Get Started</Text>
+      </Pressable>
+      <Text>Updated by Aryan Nama</Text>
+      <StatusBar style="auto" />
+      <Text>Order delicious food from here</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    // justifyContent: 'center',
+  },
+
+
+
+});
